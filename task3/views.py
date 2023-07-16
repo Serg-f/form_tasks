e2b2107c25aae6a907dbc6e67b20e0f56c25b987
serg <feelwork2077@gmail.com>
2023-07-16 20:11:38 +0200

    task3: js for + and - works well except it doesn't change form-TOTAL_FORMS property

--- task3/views.py
+++ task3/views.py
@@ -4,24 +4,20 @@ from task3.forms import TaskForm
 from .models import Task
 from django.forms import modelformset_factory
 
+TaskFormSet = modelformset_factory(model=Task, form=TaskForm)
+
 
 def create(request):
-    TaskFormSet = modelformset_factory(model=Task, form=TaskForm, extra=9)
     if request.method == 'POST':
         formset = TaskFormSet(request.POST)
         if formset.is_valid():
             formset.save()
             messages.success(request, 'Tasks created successfully.')
             return redirect('task3_create')
-        else:
-            messages.error(request, 'Error occurred while creating tasks.')
     else:
-        formset = TaskFormSet()
+        formset = TaskFormSet(queryset=Task.objects.none())
     context = {
         'formset': formset,
         'tasks': Task.objects.all()
     }
-    print('hello')
-    print(*context['tasks'])
     return render(request, 'create1.html', context)
-    # return render(request, 'test_formset.html', context)
